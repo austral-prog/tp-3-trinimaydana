f5990f4be2c98a9ab1aa5cd384f7acb6c7125a3b
trinimaydana <tmaydana@mail.austral.edu.ar>
2025-04-07 12:49:03 -0300

    Update slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -1,7 +1,18 @@
 def slice_simple():
     texto = "Awesome"
+    medio = int(len(texto)/2)
+    # Código a implementar, se debe utilizar la variable 'texto' para resolver el ejercicio.
+    # No se debe modificar la definición de la función, ni ingresar otro valor mediante input
+
+    print(texto.lower()[0:3])
+    print(texto.lower()[medio-1:medio+2])
+    print(texto.lower()[0:5]+texto.lower()[-2:])
+    
+
     # Código a implementar, se debe utilizar la variable 'texto' para resolver el ejercicio.
     # No se debe modificar la definición de la función, ni ingresar otro valor mediante input.
+
+
 
 
 # Para verificar este ejercicio ejecutar el comando

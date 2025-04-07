59aba2f08b69dedad5f4178971af182301d60c8b
trinimaydana <tmaydana@mail.austral.edu.ar>
2025-04-07 12:51:02 -0300

    Update slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -7,7 +7,6 @@ def slice_simple():
     print(texto.lower()[0:3])
     print(texto.lower()[medio-1:medio+2])
     print(texto.lower()[0:5]+texto.lower()[-2:])
-    
 
     # Código a implementar, se debe utilizar la variable 'texto' para resolver el ejercicio.
     # No se debe modificar la definición de la función, ni ingresar otro valor mediante input.

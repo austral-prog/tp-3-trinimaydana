fc12e61ef9ba12878febc6def57c431d5b57ae4d
trinimaydana <tmaydana@mail.austral.edu.ar>
2025-04-11 09:57:03 -0300

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -5,8 +5,5 @@ def slice_advanced():
     print(texto[4::2])
 
 
-
-slice_advanced()
-
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_slice_advanced_test.py` o `python tp3_slice_advanced_test.py`
